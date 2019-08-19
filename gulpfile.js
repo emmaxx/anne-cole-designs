c7cf071f40145b40f801c7987fa2d15b5d8fd653
Andy Nebs <andynebs@gmail.com>
2019-08-19 06:47:06 -0400

    basic header done

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,62 @@
 var gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    browserSync = require("browser-sync").create();
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync").create();
 
 var paths = {
-    styles: {
-        // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
-        src: "src/scss/*.scss",
-        // Compiled files will end up in whichever folder it's found in (partials are not compiled)
-        dest: "src/css"
-    }
- 
-    // Easily add additional paths
-    // ,html: {
-    //  src: '...',
-    //  dest: '...'
-    // }
+  styles: {
+    // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+    src: "src/scss/*.scss",
+    // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+    dest: "public/css"
+  }
+
+  // Easily add additional paths
+  ,html: {
+    src: 'src/*.html',
+    dest: 'public'
+  }
 };
 
 function style() {
-    return gulp
-        .src(paths.styles.src)
-        .pipe(sass())
-        .on("error", sass.logError)
-        .pipe(gulp.dest(paths.styles.dest))
-        // Add browsersync stream pipe after compilation
-        .pipe(browserSync.stream());
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(gulp.dest(paths.styles.dest))
+    // Add browsersync stream pipe after compilation
+    .pipe(browserSync.stream());
 }
 
-
-
-
-// A simple task to reload the page
-function reload() {
-    browserSync.reload();
+function html() {
+  return gulp
+    .src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browserSync.stream());
 }
 
 // Add browsersync initialization at the start of the watch task
 function watch() {
-    browserSync.init({
-        // You can tell browserSync to use this directory and serve it as a mini-server
-        server: {
-            baseDir: "./src"
-        }
-        // If you are already serving your website locally using something like apache
-        // You can use the proxy setting to proxy that instead
-        // proxy: "yourlocal.dev"
-    });
-    gulp.watch(paths.styles.src, style);
-    // We should tell gulp which files to watch to trigger the reload
-    // This can be html or whatever you're using to develop your website
-    // Note -- you can obviously add the path to the Paths object
-    //gulp.watch("src/*.html", reload);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+  browserSync.init({
+    // You can tell browserSync to use this directory and serve it as a mini-server
+    server: {
+      baseDir: "./public"
+    }
+    // If you are already serving your website locally using something like apache
+    // You can use the proxy setting to proxy that instead
+    // proxy: "yourlocal.dev"
+  });
+  gulp.watch(paths.styles.src, style);
+  // We should tell gulp which files to watch to trigger the reload
+  // This can be html or whatever you're using to develop your website
+  // Note -- you can obviously add the path to the Paths object
+  gulp.watch(paths.html.src, html);
+  //gulp.watch("src/*.html").on('change', browserSync.reload);
 }
- 
+
 // We don't have to expose the reload function
 // It's currently only useful in other functions
 
-    
+
 // Don't forget to expose the task!
 exports.watch = watch
 
@@ -70,12 +69,12 @@ exports.style = style;
  * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
  */
 var build = gulp.parallel(style, watch);
- 
+
 /*
  * You can still use `gulp.task` to expose tasks
  */
 //gulp.task('build', build);
- 
+
 /*
  * Define default task that can be called by just running `gulp` from cli
  */
